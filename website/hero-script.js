6f4bb4bf2b8e79adf4639c9ee4398c3de9623052
exihs <S10255731@connect.np.edu.sg>
2023-11-15 22:48:50 +0800

    bug fixes + more comments
    Added comments for hero-script.js
    
    Fixed a bug where the attribute button will not un-highlight when another button is pressed

--- website/hero-script.js
+++ website/hero-script.js
@@ -1,46 +1,28 @@
+
+//when button clicked, hide all elements, if tag matches the secondary class, show
 function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("heroes");
+  var items = document.getElementsByClassName("heroes");
   if (c == "all") c = "";
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show-heroes");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show-heroes");
-  }
-}
-
-// Show filtered elements
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
+  for (var i = 0; i < items.length; i++) {
+    var ele = items[i]
+    ele.className = ele.className.replace(" show-heroes","") //hides element
+    if (ele.className.indexOf(c) > -1){ //if elements have a class that matches the filter, show element
+      ele.className += " " + "show-heroes"; //apply the show-heroes class which makes them visible
     }
   }
 }
 
-// Hide elements that are not selected
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
+ //things to run when page loads
 function load(){
-  filterSelection("all")
+  filterSelection("all") //display all heroes
   var btnContainer = document.getElementById("attribute-button-area");
   var btns = btnContainer.getElementsByClassName("attribute-button");
+  //setup event listeners to give buttons an active effect
+  //when button clicked, search for active button, remove active class and give it to the button clicked
   for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function() {
-      var current = document.getElementsByClassName("active");
+      var current = btnContainer.getElementsByClassName("active");
       current[0].className = current[0].className.replace(" active", "");
       this.className += " active";
     });
